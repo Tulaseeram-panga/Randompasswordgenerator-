1f53efe5fa513dd72c01fc73143b303e6ae05950
Panga tulasee ram <99887635+Tulaseeram-panga@users.noreply.github.com>
2024-07-19 19:11:46 +0530

    Update ranpass.java

--- ranpass.java
+++ ranpass.java
@@ -1,9 +1,4 @@
 import java.security.SecureRandom;
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
 /**
  *
  * @author tulasee ram panga
